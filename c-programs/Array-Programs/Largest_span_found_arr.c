b7e3a9f2ae95d46c9bc5c39b7138daf7d54a6d53
balagithubaccount <balan7122001@gmail.com>
2022-12-31 23:00:12 +0530

    Largest span found in the leftmost and rightmost values in array

---
+++ c-programs/Array-Programs/Largest_span_found_arr.c
@@ -0,0 +1,46 @@
+   //Write a C program to find the largest span found in the leftmost and rightmost appearances of same value
+   //(values are inclusive ) in a given array...
+
+   #include<stdio.h>
+   void main()
+   {
+     int size, i, j, span, largestspan=0;
+     printf("Enter the total number of elements in an array is: ");
+     scanf("%d",&size);
+     int arr[size];
+     printf("Enter the %d elements: ",size);
+     for(i=0; i<size; i++)
+     {
+       scanf("%d",&arr[i]);
+     }
+     printf("The Given array is: ");
+     for(i=0; i<size; i++)
+     {
+       printf("%d ",arr[i]);
+     }  
+     for(i=0; i<size; i++)
+     {
+       for(j=i+1; j<size; j++)
+       {
+         if(arr[i]==arr[j])
+         {
+           span=j-i;
+           if(largestspan<span)
+           {
+             largestspan=span;
+           }
+         }
+       }
+     }
+     printf("\nThe span between the same values in the array is: %d\n",largestspan);
+   }
+  
+   /*
+   Output:
+
+   Enter the total number of elements in an array is: 9
+   Enter the 9 elements: 17 42 19 7 27 24 17 54 73
+   The Given array is: 17 42 19 7 27 24 17 54 73 
+   The span between the same values in the array is: 6
+
+   */
