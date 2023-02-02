441d4fbddad024c499e638d448bcf8fe4b8593a1
balagithubaccount <balan7122001@gmail.com>
2023-02-02 05:57:39 +0530

    String -Display  Reverse order

--- c-programs/String-programs/3-individualcharstr.c
+++ c-programs/String-programs/3-individualcharstr.c
@@ -1,14 +1,14 @@
-//Write a C program to separate the individual characters from a string.
+// Write a C program to separate the individual characters from a string.
 
 #include <stdio.h>
-int n=100;
-void split_characters(char* ptr)
+int n = 100;
+void split_characters(char *ptr)
 {
     printf("The Characters of the string are: ");
-    while(*ptr != '\0')
+    while (*ptr != '\0')
     {
-            printf("%c%c",*ptr,32);  // 32- exmpty space
-            ptr++;
+        printf("%c%c", *ptr, 32); // 32- exmpty space
+        ptr++;
     }
 }
 int main()
@@ -16,9 +16,9 @@ int main()
     char string[n];
 
     printf("Input the String: ");
-    fgets(string,sizeof(string),stdin);
+    fgets(string, sizeof(string), stdin);
 
-    printf("Given string: %s",string);
+    printf("Given string: %s", string);
     split_characters(string);
 
     return 0;
@@ -29,5 +29,5 @@ Sample Output:
 
 Input the String: Yes, You Can Do It!
 Given string: Yes, You Can Do It!
-The Characters of the string are: Y e s ,   Y o u   C a n   D o   I t ! 
+The Characters of the string are: Y e s ,   Y o u   C a n   D o   I t !
 */
