8af3077dae2611a0dae9cb08e0d65620d5dac7bb
balagithubaccount <balan7122001@gmail.com>
2023-03-02 07:59:55 +0530

    To split string by space into words

--- c-programs/String-programs/29-remove-all-spaces-str.c
+++ c-programs/String-programs/29-remove-all-spaces-str.c
@@ -1,33 +1,33 @@
-//Write a program in C to read a file and remove the spaces between two words of its content.
+// Write a program in C to read a file and remove the spaces between two words of its content.
 
 #include <stdio.h>
 #include <string.h>
-void remove_spaces(char str[],int len)
+void remove_spaces(char str[], int len)
 {
-    printf("\nThe Given string is: \n%s\n",str);
-    int i,j,count = 1;
-    while(count!=0)
+    printf("\nThe Given string is: \n%s\n", str);
+    int i, j, count = 1;
+    while (count != 0)
     {
-        count=0;
-        for(i=0; str[i]!='\0'; i++)
+        count = 0;
+        for (i = 0; str[i] != '\0'; i++)
         {
-            if(str[i]==' ')
-            {   
+            if (str[i] == ' ')
+            {
                 count++;
-                for(j=i;j<=len; j++)
+                for (j = i; j <= len; j++)
                 {
-                    str[j]=str[j+1];
+                    str[j] = str[j + 1];
                 }
             }
         }
     }
-    printf("\nAfter removing the spaces the content is: \n%s\n",str);
+    printf("\nAfter removing the spaces the content is: \n%s\n", str);
 }
 int main()
 {
     char string[100];
     printf("Input a String: ");
-    scanf(" %[^\n]",string);
+    scanf(" %[^\n]", string);
     remove_spaces(string, strlen(string));
 
     return 0;
@@ -37,10 +37,10 @@ Sample Output:
 
 Input a String: The quick brown fox jumps over the lazy dog.
 
-The Given string is: 
+The Given string is:
 The quick brown fox jumps over the lazy dog.
 
-After removing the spaces the content is: 
+After removing the spaces the content is:
 Thequickbrownfoxjumpsoverthelazydog.
 
 */
