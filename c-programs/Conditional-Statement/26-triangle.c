4a13576592a14d0b5b8eddabb863764faaed2174
balagithubaccount <balan7122001@gmail.com>
2023-05-01 08:18:54 +0530

    To check whether a triangle is equilateral, isosceles or scalene

--- c-programs/Conditional-Statement/26-triangle.c
+++ c-programs/Conditional-Statement/26-triangle.c
@@ -1,18 +1,27 @@
-//5. Write a C program to check whether a triangle is Equilateral, Isosceles or Scalene.
-// Test Data :
-// 50 50 60
-// Expected Output :
-// This is an isosceles triangle.
+// 5. Write a C program to check whether a triangle is Equilateral, Isosceles or Scalene.
+//  Test Data :
+//  50 50 60
+//  Expected Output :
+//  This is an isosceles triangle.
 
 #include <stdio.h>
-void main()
+int main()
 {
-  float a,b,c;
+  float a, b, c;
   printf("Input Three sides of triangle: \n");
-  scanf("%f%f%f",&a,&b,&c);
+  scanf("%f%f%f", &a, &b, &c);
 
-  if(a==b&&a==c){printf("This is an Equilateral triangle.\n");}
-  else if(a==b || a==c || b==c){printf("This is an Isosceles triangle.\n");}
-  else{printf("This is an Scalene triangle.\n");}
+  if (a == b && a == c)
+  {
+    printf("This is an Equilateral triangle.\n");
+  }
+  else if (a == b || a == c || b == c)
+  {
+    printf("This is an Isosceles triangle.\n");
+  }
+  else
+  {
+    printf("This is an Scalene triangle.\n");
+  }
+  return 0;
 }
-
