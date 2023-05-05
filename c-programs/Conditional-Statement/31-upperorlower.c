80446293190ca367578f4de5c1ffba389ea1eda7
balagithubaccount <balan7122001@gmail.com>
2023-05-05 07:14:17 +0530

    To check whether a given character is uppercase or lowercase

--- c-programs/Conditional-Statement/31-upperorlower.c
+++ c-programs/Conditional-Statement/31-upperorlower.c
@@ -1,15 +1,22 @@
-//4. character is uppercase or lowercase
+// 4. character is uppercase or lowercase
 
 #include <stdio.h>
 void main()
 {
-  char x;
-  printf("Enter any one character: ");
-  scanf("%c",&x);
-  
- if(x>=65 && x<=90)
-    { printf("Entered character '%c' is Uppercase alphabet.\n",x);}
- else if(x>=97 && x<=122)
-    { printf("Entered character '%c' is Lowercase alphabet.\n",x);}
- else {printf("Entered character '%c' is not alphabet\n",x);}
-}      
+   char x;
+   printf("Enter any one character: ");
+   scanf("%c", &x);
+
+   if (x >= 65 && x <= 90)
+   {
+      printf("Entered character '%c' is Uppercase alphabet.\n", x);
+   }
+   else if (x >= 97 && x <= 122)
+   {
+      printf("Entered character '%c' is Lowercase alphabet.\n", x);
+   }
+   else
+   {
+      printf("Entered character '%c' is not alphabet\n", x);
+   }
+}
