46daff43b9dfb0df1850afb453c350c96286d79d
balagithubaccount <balan7122001@gmail.com>
2023-05-08 07:57:06 +0530

    To find out  the sum of an arithmetic progress series

--- c-programs/Loop-Programs/a.p.series.c
+++ c-programs/Loop-Programs/a.p.series.c
@@ -9,24 +9,24 @@ The Sum of the A.P. series are :
 #include <stdio.h>
 void main()
 {
-  int start, terms, diff,sum=0,d,count=1,i;
+  int start, terms, diff, sum = 0, d, count = 1, i;
   printf("Input the starting number: ");
-  scanf("%d",&start);
+  scanf("%d", &start);
   printf("Input the number of terms: ");
-  scanf("%d",&terms);
+  scanf("%d", &terms);
   printf("Input the difference: ");
-  scanf("%d",&diff);
-  d=start+diff;
-  printf("%d+",start);
-  sum=sum+start;
-  for(i=start; start>0; i++)
+  scanf("%d", &diff);
+  d = start + diff;
+  printf("%d+", start);
+  sum = sum + start;
+  for (i = start; start > 0; i++)
   {
-    printf("%d+",d);
-    sum=sum+d;
-    d=d+diff;
+    printf("%d+", d);
+    sum = sum + d;
+    d = d + diff;
     count++;
-    if(count==terms)
-    break;
+    if (count == terms)
+      break;
   }
-  printf("0=%d\n",sum);
+  printf("0=%d\n", sum);
 }
