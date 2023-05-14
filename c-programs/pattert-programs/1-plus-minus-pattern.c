52c83a9936bd4a3be625f60fdebbec0c7699441b
balagithubaccount <balan7122001@gmail.com>
2023-05-14 19:36:36 +0530

    Plus-Minus pattern printing

---
+++ c-programs/pattert-programs/1-plus-minus-pattern.c
@@ -0,0 +1,58 @@
+// Write a C program to print the plus-minus pattern.
+
+#include <stdio.h>
+int main()
+{
+    int row, i, j;
+    char symbol;
+    printf("Enter the row value: ");
+    scanf("%d", &row);
+
+    for (i = 1; i <= row; i++)
+    {
+        symbol = (i % 2 == 0) ? '+' : '-';
+        for (j = 1; j <= row; j++)
+        {
+            if (i == 1 || i == row)
+            {
+                symbol = '-';
+            }
+            printf("%c ", symbol);
+            symbol = (symbol == '+') ? '-' : '+';
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+Sample Output: 1
+
+Enter the row value: 11
+- - - - - - - - - - - 
++ - + - + - + - + - + 
+- + - + - + - + - + - 
++ - + - + - + - + - + 
+- + - + - + - + - + - 
++ - + - + - + - + - + 
+- + - + - + - + - + - 
++ - + - + - + - + - + 
+- + - + - + - + - + - 
++ - + - + - + - + - + 
+- - - - - - - - - - - 
+
+Sample Output: 2
+
+Enter the row value: 8
+- - - - - - - -
++ - + - + - + -
+- + - + - + - +
++ - + - + - + -
+- + - + - + - +
++ - + - + - + -
+- + - + - + - +
+- - - - - - - -
+
+
+
+*/
